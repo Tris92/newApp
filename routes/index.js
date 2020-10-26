72f7fdc92783f46e3ed54a82f71891b515ec1deb
Eddo <eddo@macbookpro-tristan.local>
2020-10-26 17:51:48 +0100

    l'enfer sur terre

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,25 @@ const mongoose = require('mongoose');
 
 mongoose.connect("mongodb+srv://Eddo:1Landrover@codingcluster.rr02d.mongodb.net/codingsupport?retryWrites=true&w=majority",
 { useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: true,
-  useCreateIndex: true }, () =>
-  console.log("connected to DataBase")
+  useUnifiedTopology: true}, () =>
+  console.log("Connected to DataBase")
 );
 
 // <---------------------------------------------------------------------------- LOGIN / SIGNUP MODEL
 var userSchema = mongoose.Schema({
-    userName: String,
-    userEmail: String,
-    userPassword: String,
+    userName: {
+      type: String,
+      required: true,
+    },
+    userEmail: {
+      type: String,
+      required: true,
+    },
+    userPassword: {
+      type: String,
+      required: true,
+      minlength: 5
+    },
 });
 var NewUserModel = mongoose.model('newUsers', userSchema);
 var isLoggedIn = false;
@@ -41,26 +49,52 @@ router.post('/signup', function(req, res, next) {
   const {userName, userEmail, userPassword} = req.body;
   try {
     const userExist = NewUserModel.findOne({userEmail});
-    if(userExist === true) {
-      console.log(userExist)
+    if(userExist) {
       throw new Error("Cet email est déjà enregistré")
-    } else {
-      var newUser = new NewUserModel({
-        userName,
-        userEmail,
-        userPassword,
-      });
-      newUser.save(
-        function(err, newUsers) {
-          console.log(newUsers);
-          res.render('index', {newUsers, isLoggedIn});
-        }
-      )
     }
+    var newUser = new NewUserModel({
+      userName,
+      userEmail,
+      userPassword,
+    });
+    newUser.save(
+      function(err, newUsers) {
+        console.log(newUsers);
+        res.render('index', {newUsers, isLoggedIn});
+      }
+    )
   } catch(error) {
-    res.render('auth', {newUsers});
+    res.render('auth');
     console.log('Erreur ajout user')
   }
+});
+
+
+router.post('/login', function(req, res, next) {
+  const {userEmail, userPassword} = req.body;
+  {userEmail, userPassword}
+  NewUserModel.find(
+    { userEmail },
+    function (err, newUsers) {
+      if(newUsers.length > 0) {
+        req.session.user = newUsers[0];
+        isLoggedIn = true;
+          res.render('index', {user : req.session.user, isLoggedIn});
+        } else {
+          isLoggedIn = false;
+          res.render('/auth')
+        }
+    })
+});
+
+
+router.get('/logout', function(req, res, next){
+  NewUserModel.find(
+    function(err, newUsers){
+      res.render('index', { newUser: req.session.newUser, isLoggedIn, newUsers});
+      isLoggedIn = false;
+    }
+  )
 });
 
 
